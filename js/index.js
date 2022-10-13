0b3c883f1df38ad98e8eb093cdf76fb9c18a7e6d
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-10-13 15:57:08 +0200

    Finalized iteration3

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Variables and constants needed for game design
 const canvas = document.querySelector('canvas');
 canvas.style.border = "2px solid black";
 const ctx = canvas.getContext('2d');
@@ -6,6 +7,31 @@ background.src = '/images/road.png';
 const startScreen = document.querySelector('.game-intro');
 const car = new Image ();
 car.src = '/images/car.png';
+
+//Variables and constants needed for game performance
+let isGameOver = false;
+let gameId = 0;
+let isCarGoingLeft = false;
+let isCarGoingRight = false;
+let carSpeed = 3;
+let carX = canvas.width - 300;
+let carY = canvas.height - 200;
+let carWidth = 95;
+let carHeight = 100;
+
+//Functions needed for game
+const carMove = () => {
+  if (isCarGoingLeft) {
+    if (carX > 35) {
+      carX -= carSpeed;
+    }
+  } else if (isCarGoingRight) {
+    if (carX < canvas.width - carWidth - 35) {
+      carX += carSpeed;
+    }
+  }
+}
+
 
 
 
@@ -19,6 +45,29 @@ window.onload = () => {
   function startGame() {
     startScreen.style.display = 'none';
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    ctx.drawImage(car, 200, 500, 95, 100);
+    ctx.drawImage(car, carX, carY, carWidth, carHeight);
+
+    carMove();
+    if (isGameOver) {
+      cancelAnimationFrame(gameId);
+    } else {
+      gameId = requestAnimationFrame(startGame);
+    }
+    if (gameId === 1000) {
+      isGameOver = true;
+    }
   }
+  document.addEventListener("keydown", event => {
+    if (event.code === "ArrowLeft") {
+      isCarGoingLeft = true;
+    }
+    if (event.code === "ArrowRight") {
+      isCarGoingRight = true;
+    }
+  });
+
+  document.addEventListener("keyup", event => {
+    isCarGoingLeft = false;
+    isCarGoingRight = false;
+  });
 };
